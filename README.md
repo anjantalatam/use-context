93fe1691492791ba6bd97e4af9f173d19c53af34
Anjan Talatam <tspa2117@gmail.com>
2021-11-25 18:00:07 +0530

    Add github badge

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - A React-Functional-App to get famialarised with useContext hook.
 - Contexts can be used to pass data down the Component tree without POP-DRILLING.
+- Live App - https://anjantalatam.github.io/use-context
--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,23 @@ export const AuthContext = React.createContext();
 function App() {
   return (
     <div className="App">
-      I am App: I am Passing the data from here to my grand-grand Child
-      ComponentC without prop drilling
+      I am App: I am Passing data from here to my grand-grand Child ComponentC
+      without prop drilling
       <AuthContext.Provider value={"logged In"}>
         <UserContext.Provider value={"anjan"}>
           <ComponentA />
         </UserContext.Provider>
       </AuthContext.Provider>
+      <div>
+        <a
+          className="github-fork-ribbon black-bg"
+          href="https://anjantalatam.github.io/use-context"
+          data-ribbon="View Code on GitHub"
+          title="View Code on GitHub"
+        >
+          View Code on GitHub
+        </a>
+      </div>
     </div>
   );
 }
